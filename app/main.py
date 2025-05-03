832e6362c696415ec89e9ae24fb1ede585eb2857
Gabriel Ricardo <biiel06@hotmail.com>
2025-05-03 15:35:36 -0300

    add:: streamlit

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ import sys
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+import plotly.express as px
+
 
 
 url = './bd-lojas/loja_1.csv'
@@ -19,123 +21,115 @@ loja = pd.read_csv(url)
 loja2 = pd.read_csv(url2)
 loja3 = pd.read_csv(url3)
 loja4 = pd.read_csv(url4)
-
 # Análise do faturamento por loja  - Calcular a coluna Preço
  #Loja 01 --------------------------------------------------------------------------
+
 loja.head()
 faturamento_loja1 = loja['Preço'].sum()
 format_faturamento_loja1 = f"R$ {faturamento_loja1:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
 
-print('Faturamento total da loja 1: R$', format_faturamento_loja1)
 
 #Loja 02 --------------------------------------------------------------------------
 loja2.head()
 faturamento_loja2 = loja2['Preço'].sum()
 format_faturamento_loja2 = f"R$ {faturamento_loja2:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
 
-print('Faturamento total da loja 2: R$', format_faturamento_loja2)
 
 #Loja 03 --------------------------------------------------------------------------
 loja3.head()
 faturamento_loja3 = loja3['Preço'].sum()
 format_faturamento_loja3 = f"R$ {faturamento_loja3:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-print('Faturamento total da loja 3: R$', format_faturamento_loja3)
 
 #Loja 04 --------------------------------------------------------------------------
 loja4.head()
 faturamento_loja4 = loja4['Preço'].sum()
 format_faturamento_loja4 = f"R$ {faturamento_loja4:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-print('Faturamento total da loja 4: R$', format_faturamento_loja4)
 
 #-----------------------------------------------------------------------------------------------------------------
 
 # vendas por categoria
 venda_por_categoria_loja1 = loja['Categoria do Produto'].value_counts()
-print(venda_por_categoria_loja1)
 
 venda_por_categoria_loja2 = loja2['Categoria do Produto'].value_counts()
-print(venda_por_categoria_loja2)
 
 venda_por_categoria_loja3 = loja3['Categoria do Produto'].value_counts()
-print(venda_por_categoria_loja3)    
 
 venda_por_categoria_loja4 = loja4['Categoria do Produto'].value_counts()
-print(venda_por_categoria_loja4)    
 
 # média de avaliação por lojas
 
 media_avaliacao_loja1 = loja['Avaliação da compra'].mean()
 media_avaliacao_loja1 = round(media_avaliacao_loja1, 2)
-print('Média de avaliação da loja 1:', media_avaliacao_loja1)
 
 media_avaliacao_loja2 = loja2['Avaliação da compra'].mean()
 media_avaliacao_loja2 = round(media_avaliacao_loja2, 2)
-print('Média de avaliação da loja 2:', media_avaliacao_loja2)
 
 media_avaliacao_loja3 = loja3['Avaliação da compra'].mean()
 media_avaliacao_loja3 = round(media_avaliacao_loja3, 2) 
-print('Média de avaliação da loja 3:', media_avaliacao_loja3)
 
 media_avaliacao_loja4 = loja4['Avaliação da compra'].mean()         
 media_avaliacao_loja4 = round(media_avaliacao_loja4, 2)
-print('Média de avaliação da loja 4:', media_avaliacao_loja4)
 
 # produtos mais vendidos e menos vendidos
 produtos_mais_vendidos_loja1 = loja['Produto'].value_counts().head(5)
 produtos_mais_vendidos_loja1 = produtos_mais_vendidos_loja1.reset_index()
 produtos_mais_vendidos_loja1.columns = ['Produto', 'Quantidade']
-print( produtos_mais_vendidos_loja1)
 
 produto_menos_vendidos_loja1 = loja['Produto'].value_counts().tail(5)
 produto_menos_vendidos_loja1 = produto_menos_vendidos_loja1.reset_index()   
 produto_menos_vendidos_loja1.columns = ['Produto', 'Quantidade']
-print(produto_menos_vendidos_loja1)
 
 
 produtos_mais_vendidos_loja2 = loja2['Produto'].value_counts().head(5)
 produtos_mais_vendidos_loja2 = produtos_mais_vendidos_loja2.reset_index()
 produtos_mais_vendidos_loja2.columns = ['Produto', 'Quantidade']
-print( produtos_mais_vendidos_loja2)
 
 produto_menos_vendidos_loja2 = loja2['Produto'].value_counts().tail(5)
 produto_menos_vendidos_loja2 = produto_menos_vendidos_loja2.reset_index()
 produto_menos_vendidos_loja2.columns = ['Produto', 'Quantidade']
-print(produto_menos_vendidos_loja2)
 
 produtos_mais_vendidos_loja3 = loja3['Produto'].value_counts().head(5)
 produtos_mais_vendidos_loja3 = produtos_mais_vendidos_loja3.reset_index()
 produtos_mais_vendidos_loja3.columns = ['Produto', 'Quantidade']
-print( produtos_mais_vendidos_loja3)
 
 produto_menos_vendidos_loja3 = loja3['Produto'].value_counts().tail(5)
 produto_menos_vendidos_loja3 = produto_menos_vendidos_loja3.reset_index()
 produto_menos_vendidos_loja3.columns = ['Produto', 'Quantidade']
-print(produto_menos_vendidos_loja3)
 
 produtos_mais_vendidos_loja4 = loja4['Produto'].value_counts().head(5)  
 produtos_mais_vendidos_loja4 = produtos_mais_vendidos_loja4.reset_index()
 produtos_mais_vendidos_loja4.columns = ['Produto', 'Quantidade']
-print( produtos_mais_vendidos_loja4)
 
 prpoduto_menos_vendidos_loja4 = loja4['Produto'].value_counts().tail(5)
 prpoduto_menos_vendidos_loja4 = prpoduto_menos_vendidos_loja4.reset_index()
 prpoduto_menos_vendidos_loja4.columns = ['Produto', 'Quantidade']
-print(prpoduto_menos_vendidos_loja4)
 
 
 # frete médio por loja 
 frete_medio_loja1 = loja['Frete'].mean()
 frete_medio_loja1 = round(frete_medio_loja1, 2)
-print('Frete médio da loja 1:', frete_medio_loja1)
 
 frete_medio_loja2 = loja2['Frete'].mean()
 frete_medio_loja2 = round(frete_medio_loja2, 2)
-print('Frete médio da loja 2:', frete_medio_loja2)
+
 
 frete_medio_loja3 = loja3['Frete'].mean()
 frete_medio_loja3 = round(frete_medio_loja3, 2)
-print('Frete médio da loja 3:', frete_medio_loja3)
+
 
 frete_medio_loja4 = loja4['Frete'].mean()
 frete_medio_loja4 = round(frete_medio_loja4, 2)
-print('Frete médio da loja 4:', frete_medio_loja4)
+
+
+st.set_page_config(page_title="Análise de Vendas", page_icon=":bar_chart:", layout="wide")
+st.title("Análise de Vendas por Loja")
+st.subheader("Faturamento Total por Loja")
+st.write("Faturamento total da loja 1: ", format_faturamento_loja1)     
+st.write("Faturamento total da loja 2: ", format_faturamento_loja2)     
+st.write("Faturamento total da loja 3: ", format_faturamento_loja3)
+st.write("Faturamento total da loja 4: ", format_faturamento_loja4)
+
+
+st.subheader("Vendas por Categoria")
+
+
