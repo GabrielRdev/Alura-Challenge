064ceb82f22a1ea2a5f60447551c171592822297
Gabriel Ricardo <biiel06@hotmail.com>
2025-05-03 11:43:27 -0300

    commit-inicial

---
+++ app/main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+url = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-science/refs/heads/main/base-de-dados-challenge-1/loja_1.csv"
+url2 = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-science/refs/heads/main/base-de-dados-challenge-1/loja_2.csv"
+url3 = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-science/refs/heads/main/base-de-dados-challenge-1/loja_3.csv"
+url4 = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-science/refs/heads/main/base-de-dados-challenge-1/loja_4.csv"
+
+loja = pd.read_csv(url)
+loja2 = pd.read_csv(url2)
+loja3 = pd.read_csv(url3)
+loja4 = pd.read_csv(url4)
+
+# Análise do faturamento
+# vendas por categoria 
+# média de avaliação por lojas
+# produtos mais vendidos e menos vendidos
+# frete médio por loja 
+
+loja.head()
+
+loja2.head()
